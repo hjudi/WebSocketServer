d5f88c21f528c01b3686febfd79e75f06216086e
Hazim Judi <haz@born.cool>
2017-10-16 16:34:12 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "WebSocketServer",
     dependencies: [
         .Package(url: "https://github.com/Zewo/WebSocket.git", majorVersion: 0, minor: 14),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 14),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3)
     ]
 )
